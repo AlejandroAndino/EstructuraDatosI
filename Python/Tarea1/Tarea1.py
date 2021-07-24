e9afc3f525fb8fbbf7e646b54b1e76caf29f439e
AlejandroAndino <alejandro.andino@unitec.edu>
2021-07-23 21:46:33 -0600

    Tarea1 Oscar Andino passwordGenerator

---
+++ Python/Tarea1/Tarea1.py
@@ -0,0 +1,19 @@
+#Importar librerías
+import random
+import string
+
+#Crear función de generación de password
+def passwordGenerator():
+
+    #Variable string vacía
+    s = ""
+    #Lista para llamar caracteres ASCII
+    caracteres = list (string.printable)
+    #Igualar lista de caracteres para omitir los ultimos 6
+    caracteres = caracteres[:-6]
+    #Ciclo for
+    for i in range(15):
+        #Concatenación de variable string vacia con la lista
+        s += random.choice(caracteres)
+    #Imprimir contenido de la lista    
+    print (s)
